ef5d2bb5869c0f16906189258d32fca335e93073
Restyled.io <commits@restyled.io>
2020-06-29 16:22:33 +0000

    Restyled by prettier

--- webpack-hmr.config.js
+++ webpack-hmr.config.js
@@ -2,21 +2,21 @@ const webpack = require('webpack');
 const nodeExternals = require('webpack-node-externals');
 const StartServerPlugin = require('start-server-webpack-plugin');
 
-module.exports = function(options) {
+module.exports = function (options) {
   return {
     ...options,
-    entry : [ 'webpack/hot/poll?100', options.entry ],
-    watch : true,
-    externals : [
+    entry: ['webpack/hot/poll?100', options.entry],
+    watch: true,
+    externals: [
       nodeExternals({
-        whitelist : [ 'webpack/hot/poll?100' ],
+        whitelist: ['webpack/hot/poll?100'],
       }),
     ],
-    plugins : [
+    plugins: [
       ...options.plugins,
       new webpack.HotModuleReplacementPlugin(),
-      new webpack.WatchIgnorePlugin([ /\.js$/, /\.d\.ts$/]),
-      new StartServerPlugin({name : options.output.filename}),
+      new webpack.WatchIgnorePlugin([/\.js$/, /\.d\.ts$/]),
+      new StartServerPlugin({ name: options.output.filename }),
     ],
   };
 };
